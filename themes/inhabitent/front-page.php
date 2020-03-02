36c78ea3df0fbd9a90e1d01fbc3a3f1ab078ebee
JadeU <jadeunderwood3@gmail.com>
2020-03-02 11:13:58 -0800

    Add flex layout to fron page featured categories, add brand color hover state to borders

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -11,30 +11,100 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+	
 		<section class="shop-stuff-container"> 
-		  <h2 class= "journal-title">SHOP STUFF</h2>
-			<div class="do-category">
+			<h2 class= "shop-title">SHOP STUFF</h2>
 
-						<a  href="http://localhost:3000/Inhabitent/">
-								<img src="<?php echo get_template_directory_uri();?>/images/do.svg"   alt="Do category logo">
+			 <div class="product-categories-container">
+
+				<div class="do"> 
+
+					<div class="do-logo"> 
+
+						<a href="http://localhost:3000/Inhabitent/">
+								<img src="<?php echo get_template_directory_uri();?>/images/do.svg" alt="Do category logo"  >
 						</a>
-			</div>	
+					
+					</div>
 
+				<div class="do-meta">
+					<p>Get back to nature with all the tools and toys you need to enjoy the great outdoors</p>
+				</div>
 			
+				<a class="read-more-btn" href="">Do Stuff</a>
 				
+			</div>
+
+
+			<div class="eat"> 
+
+				<div class="eat-logo"> 
+
+					<a href="">
+							<img src="<?php echo get_template_directory_uri();?>/images/eat.svg"   alt="Eat category logo">
+					</a>
+
+				</div>
+
+				<div class="eat-meta">
+				<p>Nothing beats food cooked over a fire. We have all you need for good camping eats.</p>
+				</div>
+
+				<a class="read-more-btn" href="http://localhost:3000/Inhabitent/">Eat Stuff</a>
+
+			</div>
+
+		
+			<div class="sleep"> 
+
+				<div class="sleep-logo"> 
+
+					<a href="">
+							<img src="<?php echo get_template_directory_uri();?>/images/sleep.svg"   alt="Sleep category logo">
+					</a>
+
+			</div>
+
+				<div class="sleep-meta">
+					<p>Get a good night's rest in the wild in a home away from home that travels well.</p>
+				</div>
+
+				<a class="read-more-btn" href="http://localhost:3000/Inhabitent/">Sleep Stuff</a>
+
+			</div>
+
+
+
+			<div class="wear"> 
+
+				<div class="wear-logo"> 
+
+					<a href="">
+							<img src="<?php echo get_template_directory_uri();?>/images/wear.svg"   alt="Sleep category logo">
+					</a>
+
+			</div>
+
+				<div class="wear-meta">
+					<p>From flannel shirts to toques, look the part while roughing it in the great outdoors</p>
+				</div>
+
+				<a class="read-more-btn" href="http://localhost:3000/Inhabitent/">Wear Stuff</a>
+
+			</div>
+
+
+
+		
 
 
 
 
 
 
+		</div>
+	</section>
 
-
-
-
-		   <a class="read-more-btn" href="http://localhost:3000/Inhabitent/">Do Stuff</a>
-		   
-</section>
 
 
 		<section class = "featured-journal-posts">
